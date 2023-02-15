bc06bbb7eb43f24bb541644182e9b0c71dc33141
RohovAlex <47148269+RohovAlex@users.noreply.github.com>
2023-02-15 13:57:32 +0200

    writing logs to new file every day

--- main.js
+++ main.js
@@ -1,33 +1,43 @@
 'use strict';
 
 const fastify = require('fastify');
-const pinoLogger = require('pino');
 
 const path = require('node:path');
+const fs = require('node:fs');
 
 const { Logger } = require('./src/logger.js');
 const http = require('./src/http.js');
 const ws = require('./src/ws.js');
-
 const { loadApplication } = require('./src/loader.js');
+
 const APPLICATION_PATH = path.join(process.cwd(), '../NodeJS-Application');
 const LOG_FOLDER_PATH = './log';
-const date = new Date().toISOString().substring(0, 10);
-const filePath = path.join(LOG_FOLDER_PATH, `${date}.log`);
 
-const streams = [
-  { stream: process.stdout },
-  { stream: pinoLogger.destination(filePath) }
-];
-const multiLogger = pinoLogger(
-  { level: 'info' },
-  pinoLogger.multistream(streams)
-);
+const date = new Date().toISOString().substring(0, 10);
+const LOG_FILE = path.join(LOG_FOLDER_PATH, `${date}.log`);
+const logFileStream = fs.createWriteStream(LOG_FILE, { flags: 'a' });
+
+const streamForLogger = {
+  write(msg) {
+    process.stdout.write(msg);
+    const currentDate = new Date().toISOString().substring(0, 10);
+    if (currentDate === this.date) {
+      this.logFileStream.write(msg);
+    } else {
+      this.date = currentDate;
+      this.logFileStream.end();
+      const filePath = path.join(LOG_FOLDER_PATH, `${this.date}.log`);
+      this.logFileStream = fs.createWriteStream(filePath, { flags: 'a' });
+      this.logFileStream.write(msg);
+    }
+  },
+  logFileStream,
+  date,
+};
 
 (async () => {
-  const server = fastify({ logger: multiLogger });
+  const server = fastify({ logger: { level: 'info', stream: streamForLogger } });
   const logger = new Logger(server.log);
-
   const app = await loadApplication(APPLICATION_PATH, logger);
 
   http.init(server, app.api);
