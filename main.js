56dd278df53f23a5c338bd7e9b4a93edf9c56a84
Yevhen Badorov <badjorv@gmail.com>
2023-03-01 21:30:31 +0200

    Read app path from applications file

--- main.js
+++ main.js
@@ -1,25 +1,38 @@
 'use strict';
 
-const fastify = require('fastify');
+const { resolve } = require('node:path');
+const { readFile } = require('node:fs/promises');
 
-const path = require('node:path');
+const fastify = require('fastify');
 
 const { Logger, StreamForLogger } = require('./src/logger.js');
 const http = require('./src/http.js');
 const ws = require('./src/ws.js');
 const { loadApplication } = require('./src/loader.js');
 
-const APPLICATION_PATH = path.join(process.cwd(), '../NodeJS-Application');
+const APPLICATIONS_FILE_PATH = '.applications';
 const LOG_FOLDER_PATH = './log';
+
+const getAppPaths = async (appFilePath) => {
+  const appsFile = await readFile(resolve(appFilePath), {
+    encoding: 'utf8',
+  });
+
+  return appsFile.split('\n').filter((path) => path.trim() !== '');
+};
 
 (async () => {
   const streamForLogger = new StreamForLogger(LOG_FOLDER_PATH);
-  const server = fastify({ logger: { level: 'info', stream: streamForLogger } });
+  const server = fastify({
+    logger: { level: 'info', stream: streamForLogger },
+  });
   const logger = new Logger(server.log);
-  const app = await loadApplication(APPLICATION_PATH, logger);
+
+  const appPath = (await getAppPaths(APPLICATIONS_FILE_PATH))[0];
+  const app = await loadApplication(appPath, logger);
 
   http.init(server, app.api);
-  http.initStatic(server, APPLICATION_PATH);
+  http.initStatic(server, appPath);
   ws.init(server, app.api);
   http.start(server, { port: app.config.server.ports[0] });
 })();
