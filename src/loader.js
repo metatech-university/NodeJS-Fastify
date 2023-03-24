164da867febfe305f0d6a1f3214d1f8f7b04b242
Maksym Shenderuk <39469154+JaoodxD@users.noreply.github.com>
2023-03-24 22:39:08 +0300

    Update loader to fix errors with hoisted functions and vars

--- src/loader.js
+++ src/loader.js
@@ -14,7 +14,7 @@ const OPTIONS = {
 
 const load = async (filePath, sandbox) => {
   const src = await fsp.readFile(filePath, 'utf8');
-  const code = `'use strict';\n${src}`;
+  const code = `'use strict';\n{\n${src}\n}`;
   const script = new vm.Script(code);
   const context = vm.createContext(Object.freeze({ ...sandbox }));
   const exported = script.runInContext(context, OPTIONS);
