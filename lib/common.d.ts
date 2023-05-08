6cf59c67528bc3304d5dfa50c480eb5c6ad3426f
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2023-05-08 23:58:29 +0300

    Apply prettier code style

--- lib/common.d.ts
+++ lib/common.d.ts
@@ -1,4 +1,7 @@
 declare namespace common {
   function hashPassword(password: string): Promise<string>;
-  function validatePassword(password: string, serHash: string): Promise<boolean>;
+  function validatePassword(
+    password: string,
+    serHash: string,
+  ): Promise<boolean>;
 }
